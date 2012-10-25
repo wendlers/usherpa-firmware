96c907e72e1a4d0486b35a9c58f2c2e9a88d871c
Stefan Wendler <sw@kaltpost.de>
2012-10-25 23:07:28 +0200

    removed unused code

--- scratch-sherpa/src/main.c
+++ scratch-sherpa/src/main.c
@@ -32,16 +32,14 @@
  */
 
 #include <msp430.h>
-// #include <legacymsp430.h>
 
 #include "serial.h"
-// #include "conio.h"
 #include "pin.h"
 
 #define SCRATCH_DATA_REQUEST	0x01
 
 #define BAUDRATE				38400
-#define COMM_BLINK_RATE			25
+#define COMM_BLINK_RATE			15
 
 #define COMM					PIN_1_0
 #define READY					PIN_1_6
@@ -140,16 +138,6 @@ void clock_init(void)
 	__bis_SR_register(GIE);
 }
 
-#if 0
-void delay(int d) 
-{
-	volatile unsigned long i = d;
-
-	do (i--);
-	while (i != 0);
-}
-#endif
-
 /**
  * Setup the ports.
  */
@@ -199,23 +187,6 @@ void read_samples()
 
 	samples.resd.value   = 0; 
 }
-
-#if 0
-void build_scratch_pkt(unsigned char * packet, int channel, int value)
-{
-	unsigned char upper_data= (unsigned char)((value & (unsigned int)0x380) >> 7);
-	unsigned char lower_data= (unsigned char)(value & 0x7f);
-
-	*packet++ = ((1 << 7) | (channel << 3) | (upper_data));
-	*packet++ = lower_data;
-}
-
-void send_scratch_pkt(unsigned char* packet)
-{
-	serial_send_blocking(*packet++);
-	serial_send_blocking(*packet++);
-}
-#endif
 
 /**
  * Transmit a tupel consiting of a channel ID and a sample value
@@ -288,83 +259,3 @@ int main(void)
 
 	return 0;
 }
-
-
-#if 0
-int main(void)
-{
-	unsigned char cnt;
-	unsigned char c; 
-	unsigned char data_packet[] = { 0, 0 }; 
-
-	clock_init();
-
-	pin_reserve(PIN_1_1);
-	pin_reserve(PIN_1_2);
-
-	serial_init(38400);
-	cio_print("Start\n");
-
-	setup();
-	
-	cnt = 0;
-
-	for(;;) {
-
-		c = serial_recv_blocking();
-
-		if(c == SCRATCH_DATA_REQUEST) {	
-			
-			if(cnt++ == 25) {
-				// blink red led to show data was requested
-				pin_toggle(COMM);
-				cnt = 0;
-			}
-
-			// Send the ID packet
-			build_scratch_pkt(data_packet, 15, 0x04);
-			send_scratch_pkt(data_packet);
-			
-			// Read/Report channel 0 (Resistance-D)
-			build_scratch_pkt(data_packet, 0, 0);
-			send_scratch_pkt(data_packet);
-			
-			// Read/Report Channel 1 (Resistance-C)
-			build_scratch_pkt(data_packet, 1, 0);
-			send_scratch_pkt(data_packet);
-
-			// Read/Report Channel 2 (Resistance-B)
-			build_scratch_pkt(data_packet, 2, 0);
-			send_scratch_pkt(data_packet);			
-
-			// Read/Report Channel 3 (Button)
-			if(!pin_digital_read(BUTTON)) {
-				build_scratch_pkt(data_packet, 3, 0);
-			}
-			else {
-				build_scratch_pkt(data_packet, 3, 0xFFFF);
-			}
-			send_scratch_pkt(data_packet);	
-			
-			// Read/Report Channel 4 (Resistance-A)
-			build_scratch_pkt(data_packet, 4, pin_analog_read(RESA));
-			send_scratch_pkt(data_packet);		
-
-			// Read/Report Channel 5 (LIGHT)
-			build_scratch_pkt(data_packet, 5, 0);
-			send_scratch_pkt(data_packet);	
-
-			// Read/Report Channel 6 (Sound)
-			build_scratch_pkt(data_packet, 6, 0);
-			send_scratch_pkt(data_packet);			
-			
-			//Read/Report Channel 7 (Slider)
-			build_scratch_pkt(data_packet, 7, 0);
-			send_scratch_pkt(data_packet);		
-		}
-	}
-
-	return 0;
-}
-
-#endif
